ea56065d06df49e4cb2f0b522dee3f0cada61bd6
asl01 <alakoff@gmail.com>
2019-04-11 21:13:17 -0400

    Add font awesome bootstrap search components and utils folder

---
+++ client/src/utils/API.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const BASEURL = "";
+const APIKEY = "";
+
+// Export an object with a "search" method that searches the Giphy API for the passed query
+export default {
+  search: function(query) {
+    return axios.get(BASEURL + query + APIKEY);
+  }
+};
