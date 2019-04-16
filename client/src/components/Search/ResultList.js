49cbdbff998b441a1c3b8c30dacb66a932d349f4
asl01 <alakoff@gmail.com>
2019-04-16 00:00:25 -0400

    add news feature and components

--- client/src/components/Search/ResultList.js
+++ client/src/components/Search/ResultList.js
@@ -2,15 +2,13 @@ import React from "react";
 import "./search.css";
 
 function ResultList(props) {
-  console.log(props.results);
-  console.log(props.showResults);
   if (props.showResults===true) {
     return (
       <div className="table searchResults" style={{visibility: "visible" }}>
           <table className="table">
             <thead>
               <tr>
-                <th scope="col">#</th>
+                <th scope="col"></th>
                 <th scope="col">Date</th>
                 <th scope="col">Ticker</th>
                 <th scope="col">Company</th>
@@ -21,14 +19,13 @@ function ResultList(props) {
             </thead>
             <tbody>
               <tr>
-                <th scope="row">1</th>
+                <th scope="row"></th>
                 <td>{props.results.latestTime}</td>
                 <td>{props.results.symbol}</td>
                 <td>{props.results.companyName}</td>
                 <td>{props.results.sector}</td>
                 <td>{props.results.close}</td>
                 <td>{props.results.change}</td>
-  
               </tr>
             </tbody>
           </table>
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const BASEURL = "https://api.iextrading.com/1.0/stock/";
-const APIKEY = "";
 
 // Export an object with a "search" methods that searches the API for the passed stock ticker or get the news
 export default {
