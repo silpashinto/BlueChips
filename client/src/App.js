bbcaf1ca61dbe4f56202556be3d04af28abd4ce5
Silpa <silpashinto@gmail.com>
2019-04-18 20:11:30 -0400

    stock pages and news change

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/home"
+import Stock from "./pages/stock"
 import "./App.css";
 
 function App() {
-  return (
-    <div id="insider">
-      <Nav />
-      <br></br>
-      
-      <Home />
-    </div>
+  return (  
+<Router>
+<div>
+  <Nav />
+  <Switch>
+    <Route exact path="/stock/:symbol" component={Stock} />   
+  </Switch>
+  <Home />
+</div>
+</Router>
+
   );
 }
 
--- client/src/components/News/ResultList.js
+++ client/src/components/News/ResultList.js
@@ -2,56 +2,24 @@ import React from "react";
 import './news.css';
 
 function ResultList(props) {
-  console.log("news props",props);
+  console.log("news props", props);
   return (
-    <div>
-      <div className="newsHeader"> 
-        <h3> Stock Market News </h3>
+        <div id="tickr-box">
+          <div class="tickr-title">Latest News</div>
+          <div id="tickr-scroll">
+          <ul>
+          {props.results.map(result => {
+            return (
+              
+                <li><h6>{result.headline}</h6>{result.summary}</li>
+            )
+            }
+          )}
+         </ul>
+         </div>
+
       </div>
-      <div>
-        <div className="newsResults">
-      {/* <div className="table newsResults" style={{visibility: "visible" }}>
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col"></th>
-                <th scope="col">Headline</th>
-                <th scope="col">Summary</th>
-                <th scope="col">Link</th>
-                <th scope="col">Source</th>
-                <th scope="col">Image</th>
-              </tr>
-            </thead>
-            <tbody>
-            </tbody>
-            </table> */}
-            <ul id="list-example">
-              {props.results.map( result => {
-                return (
-                  <div>
-                  <li>{result.headline}{result.summary}<a href={result.url} target="_blank">{result.url}</a></li>
-
-                  <br></br>
-                  </div>
-                )
-                {/* // return (
-                //   <tr>
-                //   <th scope="row"></th>  
-                //   <td>{result.headline}</td>
-                //   <td>{result.summary}</td>
-                //   <td>{result.url}</td>
-                //   <td>{result.source}</td>
-                //   <td>{result.image}</td>
-                //   </tr>
-                // ) */}
-              })}
-            </ul>
-            </div>
-
-          
-        </div>
-    </div>
-  ) 
+  )
 }
 
 export default ResultList;
