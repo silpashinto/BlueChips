ba3e81feda175123ece0593d12e8a432523bbd3c
Silpa <silpashinto@gmail.com>
2019-04-23 20:54:50 -0400

    api routes

--- controllers/stocksController.js
+++ controllers/stocksController.js
@@ -2,7 +2,7 @@ const db = require("../models");
 
 // Defining methods for the stocksController
 module.exports = {
-  findAll: function(req, res) {
+   findAll: function(req, res) {
     db.Mystock
       .findAll()
       // .sort({ date: -1 })
@@ -12,25 +12,6 @@ module.exports = {
   findById: function(req, res) {
     db.Stock
       .findById(req.params.id)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  create: function(req, res) {
-    db.Stock
-      .create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  update: function(req, res) {
-    db.Stock
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  remove: function(req, res) {
-    db.Stock
-      .findById({ _id: req.params.id })
-      .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   }
